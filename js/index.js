519033c61fcee7fd732f6bd95e2cd3b6ffdf0afe
Allen <allenmathews416@gmail.com>
2022-09-06 22:50:11 +0300

    create a get request to the json-server

--- js/index.js
+++ js/index.js
@@ -5,4 +5,8 @@ myForm.addEventListener("submit", (event)=>{
     event.preventDefault();
 
     console.log(event.target.children[1].value);
+
+    fetch("http://localhost:3000/items/1").then(response => response.json()).then(data => {
+        console.log(data);
+    })
 })
