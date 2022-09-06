13b8a1145808aecf4ddee9bc8b2396c51f04228b
Allen <allenmathews416@gmail.com>
2022-09-06 23:21:47 +0300

    changed the inner content of the dom elements with the server data

--- js/index.js
+++ js/index.js
@@ -4,9 +4,22 @@ console.log(myForm);
 myForm.addEventListener("submit", (event)=>{
     event.preventDefault();
 
+    const userInput = event.target.children[1].value
     console.log(event.target.children[1].value);
 
-    fetch("http://localhost:3000/items/1").then(response => response.json()).then(data => {
+    const title = document.querySelector(".title");
+    const itemImage = document.querySelector(".itemImage");
+    const itemDescription = document.querySelector(".itemDescription");
+    const itemPrice = document.querySelector(".itemPrice");
+
+
+    fetch(`http://localhost:3000/items/${userInput}`).then(response => response.json()).then(data => {
         console.log(data);
+
+        title.innerHTML = data.item_name;
+        itemDescription.innerHTML = data.description;
+        itemImage.src = data.image_url;
+        itemPrice.innerHTML ="Price: " + data.item_price + " Ksh.";
+
     })
 })
